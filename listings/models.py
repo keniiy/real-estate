4252b282de58514710941c296043b811860aa5cd
kehinde kehinde peter <kehindekehinde894@gmail.com>
2021-05-22 10:31:43 +0100

    Update models.py

--- listings/models.py
+++ listings/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from django.db import models
 from realtors.models import Realtor
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
@@ -19,13 +19,13 @@ class Listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_main = CloudinaryField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = CloudinaryField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     featured = models.BooleanField(default=False)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
