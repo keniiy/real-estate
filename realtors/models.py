580207340e3c6b9d85f48d17758afb7ce978aaad
kehinde kehinde peter <kehindekehinde894@gmail.com>
2021-05-22 10:32:42 +0100

    Update models.py

--- realtors/models.py
+++ realtors/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from cloudinary.models import CloudinaryField
 from django.db import models
 
 
@@ -8,7 +8,7 @@ from django.db import models
 
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = CloudinaryField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
